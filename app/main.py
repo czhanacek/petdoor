82ac5162ab2ea31e317a50f32c99451c320d7bae
Charlie Hanacek <charlie@czhanacek.com>
2018-10-14 05:09:21 -0700

    just a test

--- app/main.py
+++ app/main.py
@@ -76,7 +76,8 @@ def evaluteThresholds():
 
 @app.route(sensors + "register", methods=["POST"])
 def register():
-    mac_address = request.args.get('mac', None)
+    mac_address = request.form.get("mac", None)
+    #print(request.data.get("mac", None))
     if(mac_address == None):
         return str(0), 500 # return error
     
@@ -100,11 +101,11 @@ def register():
 
 @app.route(sensors + "report", methods=["POST"])
 def report():
-    mac_address = request.args.get('mac', None)
+    mac_address = request.form.get('mac', None)
     if(mac_address == None):
         return str(0), 500 # return error
     
-    sensor_val = request.args.get('val', None)
+    sensor_val = request.form.get('val', None)
     if(sensor_val == None):
         return str(0), 500
     else:
