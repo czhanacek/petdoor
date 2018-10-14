4cce49189eca5b5511e7dcca507a85dc602d5c83
Charlie Hanacek <charlie@czhanacek.com>
2018-10-13 21:50:19 -0700

    lambda error

--- app/models/sensor_node.py
+++ app/models/sensor_node.py
@@ -4,7 +4,7 @@ import uuid
 class SensorNode(db.Model):
     __tablename__ = "sensor_node"
     id = db.Column(db.Integer, primary_key=True)
-    device_id = db.Column(db.Integer, default=lambda: str(uuid.uuid4().int()), 
+    device_id = db.Column(db.Integer, default=lambda: uuid.uuid4().int(), 
         nullable=False, unique=True)
     mac = db.Column(db.Integer, nullable=False)
     name = db.Column(db.String)
