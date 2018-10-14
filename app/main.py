006c35aa34589d0dcfc149947e42fce49c4e81da
Charlie Hanacek <charlie@czhanacek.com>
2018-10-14 07:28:03 -0700

    way too tired

--- app/main.py
+++ app/main.py
@@ -177,7 +177,7 @@ def get_sensors():
         num_sensors = len(sensor.sensors)
         if(num_sensors > 0):
             readings = sensor.sensors[0].readings.order_by(SensorReading.val.desc()).first()
-            if(readings.count() > 0):
+            if(readings == None):
                 theReading = readings[0].__dict__ # lists aren't mutable lol
                 theReading.pop("_sa_instance_state")
                 readingslist = theReading
